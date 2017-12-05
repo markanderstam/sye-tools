7d1931e9b88531c5af18c48cee71f62236a1fef6
xinhan <xin.han@netinsight.net>
2017-12-05 15:57:59 +0100

    Allow single server installations from Docker Hub

--- sye-cluster-leave.sh
+++ sye-cluster-leave.sh
@@ -26,13 +26,18 @@ do
     esac
 done
 
-if docker ps -a -q > /dev/null
+if [[ $(docker ps -a -q) ]]
 then
+    machineController=$(docker ps -a | grep machine-controller | awk '{ print $1 }')
+    if [[ $machineController ]]
+    then
+        docker stop $machineController
+    fi
     docker stop $(docker ps -a -q)
     docker rm -v $(docker ps -a -q)
 fi
 
-if docker volume ls -q -f dangling=true > /dev/null
+if [[ $(docker volume ls -q -f dangling=true) ]]
 then
     docker volume rm $(docker volume ls -q -f dangling=true)
 fi
